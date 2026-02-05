dde54b25c52bd1ea569a39cca2843331d981a07a
yebyn <yebyn19981114@gmail.com>
2026-02-05 15:52:58 +0900

    [FE] feat: 버튼 컴포넌트 완성

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "../../../../pages/home";
-import LoginPage from "../../../../pages/login";
-import SignUpPage from "../../../../pages/signup";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from '../../../../pages/home';
+import LoginPage from '../../../../pages/login';
+import SignUpPage from '../../../../pages/signup';
+import ButtonPage from '@/pages/components/ui/ButtonPage';
 
 const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/button" element={<ButtonPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+         </Routes>
+      </BrowserRouter>
+   );
 };
 
 export default AppRouter;
---
+++ src/pages/components/ui/ButtonPage.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button';
+import { ArrowRightIcon, PlusIcon, SearchIcon, DownloadIcon } from 'lucide-react';
+
+const ButtonPage = () => {
+   return (
+      <div className="flex flex-col gap-6 p-6 bg-white">
+         {/* 기본 버튼 */}
+         <div className="flex gap-4">
+            <Button variant="primary">Primary 버튼</Button>
+            <Button variant="primary" disabled>
+               Primary 버튼
+            </Button>
+            <Button variant="outline">Outline 버튼</Button>
+            <Button variant="outline" disabled>
+               Outline 버튼
+            </Button>
+         </div>
+
+         {/* 왼쪽 아이콘 버튼 */}
+         <div className="flex gap-2">
+            <Button variant="primary">
+               <PlusIcon /> 버튼
+            </Button>
+            <Button variant="outline">
+               <SearchIcon /> 버튼
+            </Button>
+         </div>
+
+         {/* 오른쪽 아이콘 버튼 */}
+         <div className="flex gap-2">
+            <Button variant="primary">
+               버튼 <ArrowRightIcon />
+            </Button>
+            <Button variant="outline">
+               버튼 <DownloadIcon />
+            </Button>
+         </div>
+      </div>
+   );
+};
+
+export default ButtonPage;
