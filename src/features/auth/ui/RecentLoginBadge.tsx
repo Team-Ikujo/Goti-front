df4f8aef22d40dc6260984bc3850178849be7747
kimsuhyeon <kshyunn12@gmail.com>
2026-02-11 17:17:08 +0900

    [design] 최근 로그인 뱃지 py 수치 변경 #8
    1. figma 디자인에서 보이는 padding이 badge ui 컴포넌트에 기본으로 적용된 값이기에 생략

--- src/features/auth/ui/RecentLoginBadge.tsx
+++ src/features/auth/ui/RecentLoginBadge.tsx
@@ -3,7 +3,7 @@ const RecentLoginBadge = () => {
   return (
     <Badge
       variant="default"
-      className="pointer-events-none absolute -top-2 -left-2 z-10 rounded-sm px-2 py-0.5 text-white text-[11px] font-bold bg-neutral-800"
+      className="pointer-events-none absolute -top-2 -left-2 z-10 rounded-sm px-2 py-0 text-white text-[11px] font-bold bg-neutral-800"
     >
       최근 로그인
     </Badge>
