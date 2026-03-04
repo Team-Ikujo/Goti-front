674c1df4df8f699b917e7e7c75e8cd550bfb212f
yebyn <yebyn19981114@gmail.com>
2026-03-04 11:09:29 +0900

    [feat] footer 구현

--- src/shared/widgets/layout/home/HomeLayout.tsx
+++ src/shared/widgets/layout/home/HomeLayout.tsx
@@ -1,11 +1,15 @@
+import Header from "@/shared/widgets/layout/auth/Header";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
-    <div className="min-h-screen bg-slate-950">
-      {/*TODO: 헤더*/}
-      <Outlet />
-      {/*TODO: 풋터*/}
+    <div className="min-h-screen flex flex-col bg-slate-950">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
