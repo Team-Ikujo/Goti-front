3d7fe1cf7d08b8ba9b4ba684fce6334961a88294
kimsuhyeon <kshyunn12@gmail.com>
2026-03-06 10:40:19 +0900

    [refactor] 인증 API 모듈 경로 정리 및 콜백 핸들러 의존성 정돈

--- src/features/auth/model/useSubmitAuthCode.ts
+++ src/features/auth/model/useSubmitAuthCode.ts
@@ -1,8 +1,33 @@
 import { useMutation } from "@tanstack/react-query";
-import { submitAuthCode } from "../api/submitAuthCode";
+import {
+  issueSocialState,
+  submitAuthCode,
+} from "../api/oauthApi";
+import {
+  loginWithSocialVerifyToken,
+  signupWithSocialVerifyToken,
+} from "../api/authApi";
+
+export const useIssueSocialState = () => {
+  return useMutation({
+    mutationFn: issueSocialState,
+  });
+};
 
 export const useSubmitAuthCode = () => {
   return useMutation({
     mutationFn: submitAuthCode,
+  });
+};
+
+export const useSocialLogin = () => {
+  return useMutation({
+    mutationFn: loginWithSocialVerifyToken,
+  });
+};
+
+export const useSocialSignup = () => {
+  return useMutation({
+    mutationFn: signupWithSocialVerifyToken,
   });
 };
